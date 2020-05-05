20ec91f4afdfbd55f4026a095e0c52305a7d0130
Alexey Shemyakov <IT.shemyakov@ya.ru>
2020-05-05 21:20:59 +0400

    Fix

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pygame
 from Media.colors import colors
+from Logic.trains.firstTrain import FirstTrain
 from Logic.men.testPlayer import Player
+from lib.variables import WIDTH, HEIGHT, FPS
+from searchImage import search_image
+
 import os
 
 """
 Создание основных окон и запуск модулей для последующего 
 выполнения в приложении.
 """
-
-WIDTH = 800
-HEIGHT = 600
-FPS = 30
 
 # class Window(object):
 #     """
@@ -31,7 +31,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('My Game')
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
+all_sprites.add(FirstTrain())
 all_sprites.add(Player())
+
+background = pygame.image.load(search_image('space', 'Metro.png')).convert()
+bcgd_width = background.get_rect().width
+x = 0
 
 running = True
 while running:
@@ -46,7 +51,10 @@ while running:
     all_sprites.update()
 
     # Drawing
-    screen.fill(colors.blue)
+    rel_x = x % bcgd_width
+    screen.blit(background, (rel_x - bcgd_width, 0))
+    if rel_x < WIDTH:
+        screen.blit(background, (rel_x, 0))
     all_sprites.draw(screen)
     pygame.display.flip()
 
