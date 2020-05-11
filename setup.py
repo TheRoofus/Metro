265976747324645ef6e3af3bebb177685ba30daf
Alex <skyswood@gmail.com>
2020-05-11 19:53:28 +0300

    add entity/component system

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
     maintainer=' TheRoofus ',
     description='Just for fun project. Test skills in game development.',
     include_package_data=True,
-    package_dir={'src': 'src'},
+    package_dir={'train_game': 'train_game'},
     packages=find_packages(),
-    url='https://github.com/TheRoofus/Metro/t',
-    install_requires=['pygame'],
+    url='https://github.com/TheRoofus/Metro/',
+    install_requires=['pygame', 'esper', 'pymunk'],
     version='0.1',
     entry_points={
         'console_scripts': [
